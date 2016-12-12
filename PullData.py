02234acb6889794b4bd6754175f719389400bca5
ilya <ilyaaltshteyn@gmail.com>
2016-12-12 17:29:53 +0100

    added comment to describe script

--- PullData.py
+++ PullData.py
@@ -1,9 +1,13 @@
+# This script hits the predictit API for a list of all market tickers, then for
+# each market ticker it gets all contracts within that market and loads them
+# into the all_contracts table in the predictit_db schema in MySQL.
+
 import PredictitData as pred
 import insert_contracts_data as ins
 
 
 all_market_tickers = pred.get_all_market_ids()
 
-for market_ticker in all_market_tickers[:5]:
+for market_ticker in all_market_tickers:
     all_contracts = pred.get_all_contracts(market_ticker)
     ins.insert_all_contracts(all_contracts)
