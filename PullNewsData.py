39575ba457c217ae792157f303b74ca08e8c50ed
ilya <ilyaaltshteyn@gmail.com>
2016-12-14 18:19:40 +0100

    bug fixes to prevent exceptions when loading data into sql

--- PullNewsData.py
+++ PullNewsData.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
+
 # Set up some scripts to pull newswire data into a database of headlines
 # Defining some functions to pull data from a news api.
 import pickle
 import requests
 from datetime import datetime
+import sqlalchemy as sqa
+import pandas as pd
+import hashlib
 
 with open('/home/ec2-user/predictit_assistant/news_sources.p', 'r') as infile:
     news_sources = pickle.load(infile)
 
 def sql_friendly(text):
     """ Converts apostrophes to double underscores for MySQL friendliness. """
+    if not text or text == '':
+        return 'missing__data'.encode('ascii', 'ignore')
     try:
-        return text.replace("'","__")
+        return text.replace("'", "__").replace(",", "__").replace('"', "__").replace("%", "__").encode('ascii', 'ignore')
     except:
-        return text
+        return text.encode('ascii', 'ignore')
 
 def get_news_data(source):
     """ Pulls headlines from the newsapi.org api for a given news source. """
@@ -25,9 +31,9 @@ def get_news_data(source):
 
     for art in requests.get(endpoint).json()['articles']:
 
-        description = lambda x: x['description'] if 'description' in x else 'none'
+        description = lambda x: x['description'] if 'description' in x else 'no_desc_found'
         title = art['title']
-        headline_unique_id = str(art['publishedAt']) + str(source[:min(5, len(source)/2)]) + title[:min(6, len(title)/2)]
+        headline_unique_id = hashlib.sha224(title.encode('ascii', 'ignore')).hexdigest()
 
         headlines.append((source, title, art['publishedAt'], description(art),
                           headline_unique_id))
@@ -39,23 +45,35 @@ def send_to_sql(source, con):
     """ Gets news data, checks if each headline already exists in database using the
         unique headline id. If it doesn't, adds it to the database. """
 
-    for headline in get_news_data(source):
+    news_dat = get_news_data(source)
+
+    for headline in news_dat:
+
+        if type(headline) is not tuple or len(headline) != 5:
+            return
+
         _, title, article_timestamp, description, unique_id = headline
         title = sql_friendly(title)
         description = sql_friendly(description)
         unique_id = sql_friendly(unique_id)
+        record_timestamp = sql_friendly(str(datetime.now()))
 
-        sql_statement = "select unique_id from news_headlines where unique_id='{}' limit 1".format(unique_id)
-        df = pd.read_sql(sql_statement, con)
-        if len(df) > 0:
+        sql = """SELECT unique_id FROM news_headlines WHERE unique_id='%s' LIMIT 1""" % (str(unique_id))
+        df = pd.read_sql(sql, con)
+
+        try:
+            if len(df) == 0:
+                sql = """INSERT INTO news_headlines VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')""".format(record_timestamp,
+                                                                                                                source,
+                                                                                                                article_timestamp,
+                                                                                                                title,
+                                                                                                                unique_id,
+                                                                                                                description
+                                                                                                               )
+                con.execute(sql)
+
+        except:
             continue
-
-        con.execute("INSERT INTO news_headlines VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(datetime.now(),
-                                                                                        source,
-                                                                                        article_timestamp,
-                                                                                        title,
-                                                                                        unique_id,
-                                                                                        description))
 
 
 if __name__ == "__main__":
