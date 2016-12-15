2b2befa27375e47204158488e50e25769f0cace9
ilya <ilyaaltshteyn@gmail.com>
2016-12-15 09:41:19 +0100

    initiated train data creation script and outlined its main goals

---
+++ data_and_models/create_training_data.sql
@@ -0,0 +1,4 @@
+-- This script creates a table of training data using sql, so that machine learning can be done on it quickly.
+-- The table doesn't have to be in final form, just closer to it than raw form.
+-- As part of this process, the script must join the news data to the contracts data.
+
