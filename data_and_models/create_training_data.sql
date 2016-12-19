5220026047a8969588c9b7bfb56ad5256193af51
ilya <ilyaaltshteyn@gmail.com>
2016-12-19 09:09:14 +0000

    removed unnecessary file

--- data_and_models/create_training_data.sql
+++
@@ -1,4 +0,0 @@
--- This script creates a table of training data using sql, so that machine learning can be done on it quickly.
--- The table doesn't have to be in final form, just closer to it than raw form.
--- As part of this process, the script must join the news data to the contracts data.
-
